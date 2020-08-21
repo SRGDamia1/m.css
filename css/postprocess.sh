2c9eae2239dec3f2f5d468af2eb6eba442c00114
Sara Damiano <sdamiano@stroudcenter.org>
2020-08-21 15:54:52 -0400

    Move EnviroDIY css to subfolder

--- css/postprocess.sh
+++ css/postprocess.sh
@@ -1,3 +1,7 @@
-./postprocess.py m-EnviroDIY.css
-./postprocess.py m-EnviroDIY.css m-documentation.css -o m-EnviroDIY+documentation.compiled.css
-./postprocess.py m-EnviroDIY.css m-theme-EnviroDIY.css m-documentation.css --no-import -o m-EnviroDIY.documentation.compiled.css
+./postprocess.py m-dark.css
+./postprocess.py m-dark.css m-documentation.css -o m-dark+documentation.compiled.css
+./postprocess.py m-dark.css m-theme-dark.css m-documentation.css --no-import -o m-dark.documentation.compiled.css
+
+./postprocess.py m-light.css
+./postprocess.py m-light.css m-documentation.css -o m-light+documentation.compiled.css
+./postprocess.py m-light.css m-theme-light.css m-documentation.css --no-import -o m-light.documentation.compiled.css
